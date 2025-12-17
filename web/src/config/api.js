482a55b5da93259f59e0d17ad8c949f5a97f9014
SouravBiswas238 <souravbiswas427@gmail.com>
2025-12-18 03:08:14 +0600

    update only frontend

--- web/src/config/api.js
+++ web/src/config/api.js
@@ -1,6 +1,5 @@
-// API Configuration
-export const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:5001";
+// export const API_BASE_URL = "https://api.advmhkabir.com" || "http://localhost:3001";
+export const API_BASE_URL = "https://api.advmhkabir.com";
 export const API_URL = `${API_BASE_URL}/api`;
 export const UPLOADS_URL = `${API_BASE_URL}/uploads`;
 
